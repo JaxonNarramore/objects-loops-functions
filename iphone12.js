a97dc4c54c6506ace2ff764515bb0c35f6a76dc6
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-10-21 14:36:40 -0500

    [app] add iphone12 and star-wars

---
+++ iphone12.js
@@ -0,0 +1,10 @@
+const iphone12 = {
+    image: 'path to where the image is located',
+    length: '11.28',
+    title: 'iphone 12 unboxing expierence + magsafe',
+    username: 'marques brownlee',
+    views: '5.5M',
+    uploaded: 15,
+}
+
+console.log(iphone12.title);
---
+++ star-wars.js
@@ -0,0 +1,9 @@
+const star-wars = {
+    image: 'path to where the image is located',
+    length: '60',
+    title: 'Special Look | The Mandalorian',
+    username: 'Star Wars',
+    views: '1M',
+    uploaded: '1 Day',
+    owner: 'Disney+'
+}
